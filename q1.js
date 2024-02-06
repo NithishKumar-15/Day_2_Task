ef08175fbba9c4883037ff317eb76866b27eb870
nithishkumarmurugesan2001@gmail.com <nithishkumarmurugesan2001@gmail.com>
2024-02-06 05:34:54 +0530

    Day two task completed

---
+++ q1.js
@@ -0,0 +1,12 @@
+//https://docs.google.com/document/d/1lkzy8u0rkW5v2jGvbJcJrmVVLjMqN_TmmsHkL41AoIc/preview
+/*
+Window:-
+1.)Window is a object which is used to intreact with the browser window
+2.)In the window object there are lots of method and object present in the window object
+3.)Some of the commonly used window objects and are alert , frames , and document
+Document:-
+1.)Yes document is one of the object inside the window object
+2.)Document is a object which is used to intreact with the browser document
+3.)Document is used to access the html content and to intreact with the html tags and to do some process
+4.)Some of commonly used document methods are getelementbyid, queryselector
+*/
